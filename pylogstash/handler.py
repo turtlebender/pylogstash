bb8d2bf66381fb4fd88ce06d700cb5bbfa98376d
Tom Howe <turtlebender@gmail.com>
2012-03-06 10:52:46 -0600

    fixing tests

--- pylogstash/handler.py
+++ pylogstash/handler.py
@@ -11,12 +11,13 @@ class Handler(logging.Handler):
     """ A logging handler for sending notifications to a 0mq PUSH.
 
     """
-    def __init__(self, connect_strings=['tcp://127.0.0.1:2120'], fields=[],
+    def __init__(self, connect_string="tcp://127.0.0.1:2120", fields=[],
                  tags=[], input_type='pylogstash', context=None, queue_length=MAX_MESSAGES):
         logging.Handler.__init__(self)
-        self._context = context if context is not None else zmq.Context()
+        self._sockets = []
+        self._context = context if context is not None else zmq.Context.instance()
         self._tags = tags
-        self._connect_strings = connect_strings
+        self._connect_string = connect_string
         self._fields = fields
         self._input_type = input_type
         self._local = threading.local()
@@ -25,13 +26,12 @@ class Handler(logging.Handler):
     @property
     def publisher(self):
         if not hasattr(self._local, 'publisher'):
+            print("creating publisher")
             # 0mq sockets aren't threadsafe, so bind them into a
             # threadlocal
             self._local.publisher = self._context.socket(zmq.PUB)
             self._local.publisher.setsockopt(zmq.HWM, self._queue_length)
-
-            for connect_string in self._connect_strings:
-                self._local.publisher.connect(connect_string)
+            self._local.publisher.connect(self._connect_string)
         return self._local.publisher
 
     def emit(self, record):
@@ -54,3 +54,4 @@ class Handler(logging.Handler):
         }
         print("Shipping log")
         self.publisher.send_json(message)
+        print("message shipped")
--- tests/pylogstash_tests.py
+++ tests/pylogstash_tests.py
@@ -1,33 +1,51 @@
 from __future__ import absolute_import
 import zmq
-import multiprocessing
+from threading import Thread
 import logging
+import json
+from unittest import TestCase
+import time
+import sys
+
+from zmq.utils.strtypes import asbytes
 from pylogstash import LogstashHandler
 
 
-def subscribe(queue):
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.bind("tcp://127.0.0.1:22222")
-    socket.setsockopt(zmq.SUBSCRIBE, "")
-    queue.put(socket.recv())
+class PyLogstashTest(TestCase):
 
+    def setUp(self):
+        self.sockets = []
+        self.context = zmq.Context.instance()
+        self.listener = zmq.Socket(self.context, zmq.SUB)
+        self.listener.setsockopt(zmq.LINGER, 0)
+        self.listener.setsockopt(zmq.SUBSCRIBE,asbytes(''))
+        self.port = self.listener.bind_to_random_port("tcp://127.0.0.1")
+        self.handler = LogstashHandler("tcp://127.0.0.1:{0}".format(self.port), context=self.context)
+        self.handler.publisher.setsockopt(zmq.LINGER, 0)
+        self.sockets.extend([self.listener, self.handler.publisher])
+        time.sleep(0.1)
 
-def test():
-    queue = multiprocessing.Queue()
-    process = multiprocessing.Process(target=subscribe, args=(queue, ))
-    process.start()
+    def tearDown(self):
+        contexts = set([self.context])
+        while self.sockets:
+            sock = self.sockets.pop()
+            contexts.add(sock.context) # in case additional contexts are created
+            sock.close(0)
+        for ctx in contexts:
+            t = Thread(target=ctx.term)
+            t.daemon = True
+            t.start()
+            t.join(timeout=2)
+            if sys.version[:3] == '2.5':
+                t.is_alive = t.isAlive
+            if t.is_alive():
+                # reset Context.instance, so the failure to term doesn't corrupt subsequent tests
+                zmq.core.context._instance = None
+                raise RuntimeError("context could not terminate, open sockets likely remain in test")
 
-    log = logging.getLogger()
-    handler = LogstashHandler()
-    log.addHandler(handler)
-    log.setLevel(logging.DEBUG)
-    log.warn("hello")
-    process.join()
-    import json
-    message = json.loads(queue.get())
-    assert message['@message'] == "hello"
-
-
-if __name__ == "__main__":
-    test()
+    def test_basic_logging(self):
+        log = logging.getLogger()
+        log.addHandler(self.handler)
+        log.setLevel(logging.DEBUG)
+        log.info("hello")
+        self.listener.recv()
